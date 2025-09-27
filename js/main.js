e3842aa24e5231dc9c3d928e45cfc48959fa2a75
gkdbwjd0820 <gkdbwjd0820@gmail.com>
2025-09-27 20:47:17 +0900

    섹션2 ing
    이미지 상품명 바꾸기

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+    var swiper = new Swiper(".sec_2", {
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+});
