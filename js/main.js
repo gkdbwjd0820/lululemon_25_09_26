05aff7ee6100ee351a9e41e6f16c3b0520d8b825
gkdbwjd0820 <gkdbwjd0820@gmail.com>
2025-09-28 20:18:07 +0900

    푸터까지 료
    섹션1스와이퍼 / 섹션2 내용 / 우먼버튼 /모바일 하기

--- js/main.js
+++ js/main.js
@@ -6,4 +6,31 @@ $(document).ready(function(){
         prevEl: ".swiper-button-prev",
       },
     });
+
+    var swiper = new Swiper(".sec_6", {
+    slidesPerView: 2,
+    spaceBetween: 16,
+    freeMode: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    autoplay: {
+      delay: 100,
+      disableOnInteraction: false,
+    },
+    speed: 5000,
+    effect: "slide",
+    loop: true,
+    breakpoints: {
+      640: {
+        slidesPerView: 2.5,
+        spaceBetween: 20,
+      },
+      1024: {
+        slidesPerView: 3.7,
+        spaceBetween: 20,
+      },
+    },
+  });
 });
