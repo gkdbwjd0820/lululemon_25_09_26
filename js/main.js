5084480fda3a4b064ebe13281d6bebedca17649a
xxxxx___0___xxxxx <skmm191919@gmail.com>
2025-09-30 12:50:22 +0900

    수정하기
    메뉴바에 메뉴 만들기

--- js/main.js
+++ js/main.js
@@ -41,39 +41,73 @@ $(document).ready(function(){
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
       },
+        observer: true,
+        observeParents: true,
     });
 
     var swiper = new Swiper(".mo_sec_2", {
       pagination: {
         el: ".swiper-pagination",
+        clickable: true,
       },
+
+        loop: false,
+        observer: true,
+        observeParents: true,
     });
     
 
-    var swiper = new Swiper(".sec_6", {
-    slidesPerView: 2,
-    spaceBetween: 16,
-    freeMode: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    autoplay: {
-      delay: 100,
-      disableOnInteraction: false,
-    },
-    speed: 5000,
-    effect: "slide",
-    loop: true,
-    breakpoints: {
-      640: {
-        slidesPerView: 2.5,
-        spaceBetween: 20,
-      },
-      1024: {
-        slidesPerView: 3.7,
-        spaceBetween: 20,
-      },
-    },
+  //   var swiper = new Swiper(".sec_6", {
+  //   slidesPerView: 2,
+  //   spaceBetween: 16,
+  //   freeMode: true,
+  //   pagination: {
+  //     el: ".swiper-pagination",
+  //     clickable: true,
+  //   },
+  //   autoplay: {
+  //     delay: 100,
+  //     disableOnInteraction: false,
+  //   },
+  //   speed: 5000,
+  //   effect: "slide",
+  //   loop: true,
+  //   breakpoints: {
+  //     640: {
+  //       slidesPerView: 2.5,
+  //       spaceBetween: 20,
+  //     },
+  //     1024: {
+  //       slidesPerView: 3.7,
+  //       spaceBetween: 20,
+  //     },
+  //   },
+  // });
+
+  $(".bxslider1").bxSlider({
+
+    minSlides: 3,
+    maxSlides: 5,
+    slideWidth: 540,
+    slideMargin: 40,
+    ticker: true,
+    speed: 9000
+
   });
+
+  $(".bxslider2").bxSlider({
+
+    minSlides: 1,
+    maxSlides: 3,
+    slideWidth: 260,
+    slideMargin: 10,
+    ticker: true,
+    speed: 9000
+
+  });
+
+
+  
+
+  
 });
